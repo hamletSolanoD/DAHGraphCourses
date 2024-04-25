7cb0543b818d0b819d340c20c64ea407c5699b54
hamletSolanoD <85127132+hamletSolanoD@users.noreply.github.com>
2024-04-25 13:23:10 -0600

    first commit

---
+++ src/main/java/com/DAH/graphEducation/DahCoursesApplication.java
@@ -0,0 +1,13 @@
+package com.DAH.graphEducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DahCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DahCoursesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DAH/graphEducation/DahCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DAH.graphEducation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DahCoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
